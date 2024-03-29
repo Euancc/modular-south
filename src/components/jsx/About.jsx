f768f61221811e6e788ce09ef11144439a11ed90
Euan Cunningham <euan@Asus-Lap>
2024-03-30 06:10:52 +1100

    background colors applied

--- src/components/jsx/About.jsx
+++ src/components/jsx/About.jsx
@@ -3,23 +3,25 @@ import '../css/about.css'
 
 function About() {
   return (
-    <div className="about-container">
-      <div className="about-text">
-        <h1>Who we are</h1>
-        Modular South is a new business using Licensed Building Practitioner
-        carpenters with 10+ years knowledge in the construction industry to
-        build the right Transportable dwellings for you. With rising house
-        prices, high inflation rates and material shortages our goal is to
-        provide Kiwis with an alternative way of living. If you are looking for
-        an extra space for your property or a cosy bach away from home, we
-        provide architecturally designed, Transportable dwellings, Sleepouts of
-        various sizes to suit your needs. Our units are ideal as they are, or
-        can be added onto each other to create the ideal space. Our units are
-        made off site on the West Coast ready for delivery anywhere around the
-        South Island.
+    <section className="about-section">
+      <div className="about-container">
+        <div className="about-text">
+          <h1>Who we are</h1>
+          Modular South is a new business using Licensed Building Practitioner
+          carpenters with 10+ years knowledge in the construction industry to
+          build the right Transportable dwellings for you. With rising house
+          prices, high inflation rates and material shortages our goal is to
+          provide Kiwis with an alternative way of living. If you are looking
+          for an extra space for your property or a cosy bach away from home, we
+          provide architecturally designed, Transportable dwellings, Sleepouts
+          of various sizes to suit your needs. Our units are ideal as they are,
+          or can be added onto each other to create the ideal space. Our units
+          are made off site on the West Coast ready for delivery anywhere around
+          the South Island.
+        </div>
+        <img className="about-img" src="ModSFraming4.3.jpg" />
       </div>
-      <img className="about-img" src="ModSFraming4.3.jpg" />
-    </div>
+    </section>
   )
 }
 
--- src/components/jsx/PriceCards.jsx
+++ src/components/jsx/PriceCards.jsx
@@ -4,38 +4,40 @@ import '../css/priceCards.css'
 function PriceCards() {
   return (
     <>
-      <h1>Price List</h1>
-      <div className="card-container">
-        <div className="card">
-          <img className="card-img" src="CardImage7.5x4.jpg" />
-          <div className="card-text">
-            <h2>7.5mx4.0m Modular unit starting from $130,000</h2>
-            <div>Self Contained, Transportable dwellings Sleepouts/flat </div>
+      <section className="card-section">
+        <h1>Price List</h1>
+        <div className="card-container">
+          <div className="card">
+            <img className="card-img" src="CardImage7.5x4.jpg" />
+            <div className="card-text">
+              <h2>7.5mx4.0m Modular unit starting from $130,000</h2>
+              <div>Self Contained, Transportable dwellings Sleepouts/flat </div>
+            </div>
           </div>
-        </div>
 
-        <div className="card">
-          <img className="card-img" src="CardImage7.5x4.jpg" />
-          <div className="card-text">
-            <h2>6.0mx3.6m Modular unit starting from $125,000</h2>
-            <div>Self Contained, Transportable dwellings Sleepouts/flat </div>
+          <div className="card">
+            <img className="card-img" src="CardImage7.5x4.jpg" />
+            <div className="card-text">
+              <h2>6.0mx3.6m Modular unit starting from $125,000</h2>
+              <div>Self Contained, Transportable dwellings Sleepouts/flat </div>
+            </div>
           </div>
-        </div>
 
-        <div className="card">
-          <img className="card-img" src="CardImage7.5x4.jpg" />
-          <div className="card-text">
-            <h2>4.8.mx3.6m Modular unit starting from $73,000</h2>
+          <div className="card">
+            <img className="card-img" src="CardImage7.5x4.jpg" />
+            <div className="card-text">
+              <h2>4.8.mx3.6m Modular unit starting from $73,000</h2>
+            </div>
           </div>
-        </div>
 
-        <div className="card">
-          <img className="card-img" src="CardImage7.5x4.jpg" />
-          <div className="card-text">
-            <h2>3.6.mx3.6m Modular unit starting from $55,000</h2>
+          <div className="card">
+            <img className="card-img" src="CardImage7.5x4.jpg" />
+            <div className="card-text">
+              <h2>3.6.mx3.6m Modular unit starting from $55,000</h2>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   )
 }
